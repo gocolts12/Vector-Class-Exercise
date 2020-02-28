cf71f3de3fa30d40fab3db20f67cff1770472175
Michael <mlederer101@gmail.com>
2020-02-28 16:23:26 -0600

    push_back

--- practice/practice/practice.cpp
+++ practice/practice/practice.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <vld.h>
 
 template <typename T>
 class Vector
@@ -9,22 +10,24 @@ class Vector
 private: 
 	std::allocator<T> alloc; 
 	T* vec;
-	int size;
+	int capacity;
+	int numValuesFilled;
 
 public:
 	Vector()
 	{
 		std::cout << "default constructor called" << std::endl;
-		
+		capacity = 0;
+		numValuesFilled = 0;
 	}
 	Vector(const int &designatedSize)
 	{
 		std::cout << "size-defined constructor called" << std::endl;
-		size = designatedSize;
-		vec = alloc.allocate(size);
-		for (int i = 0; i < size; i++)
+		capacity = designatedSize;
+		numValuesFilled = 0;
+		vec = alloc.allocate(capacity);
+		for (int i = 0; i < capacity; i++)
 		{
-			//alloc.construct(vec[i], 0);
 			vec[i] = 0;
 		}
 	}
@@ -33,7 +36,7 @@ public:
 		std::cout << "copy constructor called" << std::endl;
 		int copySize = vecToCopy.Size();
 
-		this.size = copySize;
+		capacity = copySize;
 		vec = alloc.allocate(copySize);
 		for (int i = 0; i < copySize; i++)
 		{
@@ -41,20 +44,44 @@ public:
 		}
 	}
 
-	//Vector()
-	//{
-	//	std::cout << "move constructor called" << std::endl;
-	//	
-	//}
+	void push_back(T val)
+	{
+		if (capacity == 0)
+		{
+			capacity = 1;
+			vec = alloc.allocate(1);
+			vec[0] = val;
+			++numValuesFilled;
+		}
+		else if (numValuesFilled >= capacity)
+		{
+			resize();
+			vec[numValuesFilled] = val;
+			numValuesFilled++;
+		}
+		else
+		{
+			vec[numValuesFilled] = val;
+			numValuesFilled++;
+		}
+		
+	}
 
-
+	void resize()
+	{
+		T* tempVec;
+		tempVec = alloc.allocate(capacity * 2);
+		memcpy(tempVec, vec, numValuesFilled * sizeof(T));
+		alloc.deallocate(vec, capacity);
+		vec = tempVec;
+		capacity *= 2;
+	}
 
 	int Size()
 	{
-		return size;
+		return numValuesFilled;
 	}
 
-	friend std::ostream& operator<<(std::ostream& stream, const Vector& mikeVector);
 
 	const T& at(int index) const
 	{
@@ -92,14 +119,44 @@ public:
 
 	~Vector()
 	{
-		alloc.deallocate(vec, size);
+		alloc.deallocate(vec, capacity);
 		alloc.destroy(vec);
 	}
 };
 
 int main()
 {
-	Vector<int> myVec(3);
-	std::cout << myVec[0];
+	Vector<int> myVec;
+	myVec.push_back(1);
+	myVec.push_back(2);
+	myVec.push_back(3);
+	myVec.push_back(4);
+	myVec.push_back(5);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	myVec.push_back(6);
+	for (int i = 0; i < myVec.Size(); i++) std::cout << myVec.at(i);
+
 
 }
